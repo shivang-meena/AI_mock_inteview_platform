7a086c0c2362ce7e7d63cff21dbbb23bbf4536a9
shivang meena <shivang340908@gmail.com>
2026-08-17 18:28:18 +0530

    prisma issue fixed of building in production

--- client/src/lib/Api.ts
+++ client/src/lib/Api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.git  || "http://localhost:5000";
+const API_URL = process.env.git  || "https://ai-mock-inteview-platform.onrender.com";
 
 interface ApiOptions {
   method?: string;
